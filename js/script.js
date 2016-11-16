8dea418e0620f6c9761f66da36132280d99b5dce
Morgner, Andreas (Scandio) <andreas.morgner@scandio.de>
2016-11-16 15:36:03 +0100

    added wether gadget and some more icons

--- js/script.js
+++ js/script.js
@@ -41,14 +41,14 @@ var settings = {
 	
 	"search": {
 		"engines": [
-			["http://www.google.com/search", "q", "Google", "sg"],
-			["http://www.google.com/images", "q", "Google Images", "si"],
-			["http://search.yahoo.com/search", "p", "Yahoo", "sy"],
-			["http://wikipedia.org/w/index.php", "w", "Wikipedia", "sw"],
-			["http://www.dict.cc", "s", "dict.cc", "sd"],
-			["http://dict.leo.org", "search", "leo", "sl"],
-			["http://www.flickr.com/search", "q", "flickr", "sf"],
-			["http://browse.deviantart.com/", "q", "deviantArt", "sa"]
+			["https://www.google.com/search", "q", "Google", "sg"],
+			// ["http://www.google.com/images", "q", "Google Images", "si"],
+			// ["http://search.yahoo.com/search", "p", "Yahoo", "sy"],
+			// ["http://wikipedia.org/w/index.php", "w", "Wikipedia", "sw"],
+			["http://www.dict.cc", "s", "Dict.cc", "sd"],
+			// ["http://dict.leo.org", "search", "leo", "sl"],
+			// ["http://www.flickr.com/search", "q", "flickr", "sf"],
+			// ["http://browse.deviantart.com/", "q", "deviantArt", "sa"]
 		],
 		"focusSearch": false
 	},
@@ -56,7 +56,9 @@ var settings = {
 	"clock": {
 		"showClock": true
 	},
-
+	"weather": {
+		"showWether": true
+	},
 	"animation": {
 		"hideLinks": true
 	},
@@ -72,23 +74,24 @@ function updateClock() {
 	var currentTime = new Date ();
 	var currentHours = currentTime.getHours ();
 	var currentMinutes = currentTime.getMinutes ();
-	var currentSeconds = currentTime.getSeconds ();
+	//var currentSeconds = currentTime.getSeconds ();
 
 	// Pad the minutes and seconds with leading zeros, if required
 	currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
-	currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+	//currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
 
 	// Choose either "AM" or "PM" as appropriate
 	var timeOfDay = (currentHours < 12) ? "AM" : "PM";
+	var timeOfDay = ""
 
 	// Convert the hours component to 12-hour format if needed
-	currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
+	// currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
 
 	// Convert an hours component of "0" to "12"
 	currentHours = (currentHours == 0) ? 12 : currentHours;
 
 	// Compose the string for display
-	var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+	var currentTimeString = currentHours + ":" + currentMinutes;
 
 	// Fill '#clock' div with time
 	$("#clock").html(currentTimeString);
@@ -96,11 +99,44 @@ function updateClock() {
 
 function searchBox(url, name, placeholder) {
 	var string = '<form method="get" action="' + url + '">'
-	           + '<input type="text" id="g" name="' + name + '" placeholder="' + placeholder + '" maxlength="255" value="">'
+	           + '<input type="text" id="g" name="' + name + '" placeholder="' + "" + '" maxlength="255" value="">'
+	           + '<label for="g">'+placeholder+'</label>'
 	           + '<input type="submit" value="Go">'
 	           + '</form>';
 	return string;
 }
+
+
+/* Greeding *\
+\* ======== */
+(function ($) {
+	var username = "Andreas"
+  $.fn.setGreeting = function(username){
+    var now = new Date().getHours();
+    var text;
+
+    if (now >= 6 && now < 12){
+      text = 'Guten Morgen';
+    }
+    else if (now >= 12 && now < 17){
+      text = 'Hallo'; 
+    }
+    else if (now >= 17 && now < 22 ){
+      text = 'Guten Abend';
+    }
+    else{
+      text = 'Gute Nacht';
+    }
+
+    return this.each(function(){
+      var $div = $(this);
+
+      $div.html(text + ' ' + username);
+    });
+  }
+
+}(jQuery));
+
 
 $(document).ready(function() {
 
@@ -235,13 +271,100 @@ $(document).ready(function() {
  
 	/*  Clock  *\
 	\*=========*/
-
 	if(settings.clock.showClock) {
+
+		var current_location = ""
+
 		// Add empty '#clock' div
 		$('body').append('<div id="clock"></div>');
-
+		$('<div id="greeting"></div>').insertAfter('div#clock');
 		// Update clock
-		setInterval('updateClock()', 1000);
+		setInterval('updateClock()', 100);
+		$('#greeting').setGreeting('Andreas');
+
+		$('<div id="weather"><img src="images/loading3.gif" width="100" /></div>').insertAfter('div#greeting');
+		$.getJSON('https://freegeoip.net/json/').done(function(geo_location) {
+			$('#loadingmessage').show();
+			/*
+			console.log(geo_location.country_name);
+			console.log(geo_location.country_code);
+			console.log(geo_location.region_name);
+			console.log(geo_location.region_code);
+			console.log(geo_location.city);
+			console.log(geo_location.latitude);
+			console.log(geo_location.longitude);
+			console.log(geo_location.time_zone);
+			console.log(geo_location.ip);
+			*/
+		
+			$.simpleWeather({
+			    location: geo_location.city,
+			    woeid: '',
+			    unit: 'c',
+			    success: function(weather) {
+			    	var codes = new Array();
+
+					codes[0] = "Wirbelsturm"; //"tornado"
+					codes[1] = "Tropensturm"; //"tropical storm"
+					codes[2] = "Orkan"; //"hurricane"
+					codes[3] = "schwere Gewitter"; //"severe thunderstorms"
+					codes[4] = "Gewitter"; //"thunderstorms"
+					codes[5] = "Regen und Schnee"; //"mixed rain and snow"
+					codes[6] = "Regen und Graupelschauer"; //"mixed rain and sleet"
+					codes[7] = "Schnee und Graupelschauer"; //"mixed snow and sleet"
+					codes[8] = "Nieselregen gefrierend"; //"freezing drizzle"
+					codes[9] = "Nieselregen"; //"drizzle"
+					codes[10] = "Eisregen"; //"freezing rain"
+					codes[11] = "Regenschauer"; //"showers"
+					codes[12] = "Regen"; //"showers"
+					codes[13] = "Schneegestöber"; //"snow flurries"
+					codes[14] = "leichte Schneeschauer"; //"light snow showers"
+					codes[15] = "Schneetreiben"; // "blowing snow"
+					codes[16] = "Schnee"; //"snow"
+					codes[17] = "Hagel"; //"hail"
+					codes[18] = "Schneeregen"; //"sleet"
+					codes[19] = "Staub"; //"dust"
+					codes[20] = "neblig"; //"foggy"
+					codes[21] = "Dunst"; //"haze"
+					codes[22] = "rauchig"; //"smoky"
+					codes[23] = "stürmisch"; //"blustery"
+					codes[24] = "windig"; //"windy"
+					codes[25] = "kalt"; //"cold"
+					codes[26] = "bewölkt"; //"cloudy"
+					codes[27] = "Nachts überwiegend bewölkt"; //"mostly cloudy (night)"
+					codes[28] = "Tags überwiegend bewölkt"; //"mostly cloudy (day)"
+					codes[29] = "Nachts wechselnd bewölkt"; //"partly cloudy (night)"
+					codes[30] = "Tags wechselnd bewölkt"; //"partly cloudy (day)"
+					codes[31] = "Nachts klar"; //"clear (night)"
+					codes[32] = "sonnig"; //"sunny"
+					codes[33] = "Nachts schön"; //"fair (night)"
+					codes[34] = "Tags schön"; //"fair (day)"
+					codes[35] = "Regen und Hagel"; //"mixed rain and hail"
+					codes[36] = "heiss"; //"hot"
+					codes[37] = "vereinzelt Gewitter"; //"isolated thunderstorms"
+					codes[38] = "vereinzelte Gewitter"; //"scattered thunderstorms"
+					codes[39] = "vereinzelte Gewitter"; //"scattered thunderstorms"
+					codes[40] = "vereinzelte Schauer"; //"scattered showers"
+					codes[41] = "schwerer Schneefall"; //"heavy snow"
+					codes[42] = "vereinzelte Schneeschauer"; //"scattered snow showers"
+					codes[43] = "schwerer Schneefall"; //"heavy snow"
+					codes[44] = "wechselnd bewölkt"; //"partly cloudy"
+					codes[45] = "Gewitterschauer"; //"thundershowers"
+					codes[46] = "Schneeschauer"; //"snow showers"
+					codes[47] = "vereinzelt Gewitterschauer"; //"isolated thundershowers"
+					codes[3200] = "nicht verfügbar"; //"not available"
+			    	html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+			    	html += '<ul><img style="float:left;" width="50px" src="'+weather.image+'" /><li>'+weather.city+', '+weather.region+'</li>';
+			    	html += '<li class="currently">'+codes[weather.code]+'</li>';
+			    	html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+			    	$('#loadingmessage').hide();
+			    	$("#weather").html(html);
+			    },
+			    error: function(error) {
+			      $("#weather").html('<p>'+error+'</p>');
+			    }
+			});
+		});
 	}
 
 
